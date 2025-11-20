5ab71684c6b1dbf97036ef19bd20326726332904
Wilker Rwaik Eloi <wilkermid@gmail.com>
2025-11-20 19:08:18 -0300

    feat: Adiciona suporte a upload de imagens e documentação da API

--- src/infra/routes/upload-image.ts
+++ src/infra/routes/upload-image.ts
@@ -3,7 +3,11 @@ import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 export const uploadImageRoute: FastifyPluginAsyncZod = async (
   server
 ) => {
-  server.post('/uploads', () => {
+  server.post('/uploads',{
+    schema:{
+        summary: "Upload an image"
+    }
+  } , async () => {
     return 'Hello World'
   })
 }
