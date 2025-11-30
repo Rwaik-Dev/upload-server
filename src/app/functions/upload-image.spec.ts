df7387c14d2414d4dde25aa24ae2ab6fde823081
Wilker Rwaik Eloi <wilkermid@gmail.com>
2025-11-30 01:00:38 -0300

    feat: Adiciona validação de tamanho de arquivo e testes para a função de upload de imagem

---
+++ src/app/functions/upload-image.spec.ts
@@ -0,0 +1,58 @@
+import { randomUUID } from 'node:crypto'
+import { Readable } from 'node:stream'
+import { eq } from 'drizzle-orm'
+import { beforeAll, describe, expect, it, vi } from 'vitest'
+import { db } from '@/infra/db'
+import { schema } from '@/infra/db/schemas'
+import { isLeft, isRight, unwrapEither } from '@/infra/shared/either'
+import { uploadImage } from './upload-image'
+import { InvalidFileFormat } from '../erros/invalid-file-format'
+
+describe('uploadImage', () => {
+  beforeAll(() => {
+    vi.mock('@/infra/storage/upload-file-to-storage', () => {
+      return {
+        uploadFileToStorage: vi.fn().mockImplementation(() => {
+          return {
+            key: `${randomUUID()}.jpg`,
+            url: 'https://example.com/image.jpg',
+          }
+        }),
+      }
+    })
+  })
+
+  it('should be able to upload an image', async () => {
+    const fileName = `${randomUUID()}.jpg`
+
+    const sut = await uploadImage({
+      fileName: fileName,
+      contentType: 'image/jpeg',
+      contentStream: Readable.from([]),
+    })
+
+    expect(isRight(sut)).toBe(true)
+
+    const result = await db
+      .select()
+      .from(schema.uploads)
+      .where(eq(schema.uploads.name, fileName))
+
+    expect(result).toHaveLength(1)
+  })
+
+  it('should not be able to upload an invalid file', async () => {
+    const fileName = `${randomUUID()}.pdf`
+
+    const sut = await uploadImage({
+      fileName: fileName,
+      contentType: 'document/pdf',
+      contentStream: Readable.from([]),
+    })
+
+    expect(isLeft(sut)).toBe(true)
+
+    expect(unwrapEither(sut)).toBeInstanceOf(InvalidFileFormat)
+
+  })
+})
